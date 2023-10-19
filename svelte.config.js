eb4952eb5d7fbc17ee955f7dad2a62606e218d59
GHOST <ghostdevbusiness@gmail.com>
2023-10-19 19:07:27 +0000

    chore: configure netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,13 @@
-import preprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-netlify';
+import preprocess from 'svelte-preprocess';
 
+/**
+ * @type {import('@sveltejs/kit').Config}
+ */
 export default {
-    preprocess: [preprocess()]
-}
+	preprocess: [preprocess()],
+
+	kit: {
+		adapter: adapter(),
+	},
+};
