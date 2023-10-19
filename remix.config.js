3fd0d7360acfe6aed1d3f9a030e00cab919aee53
GHOST <ghostdevbusiness@gmail.com>
2023-10-19 18:27:41 +0000

    refactor: init SvelteKit

--- remix.config.js
+++
@@ -1,9 +0,0 @@
-/**
- * @type {import('@remix-run/dev').AppConfig}
- */
-export default {
-    cacheDirectory: './node_modules/.cache/remix',
-    ignoredRouteFiles: ['**/.*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}'],
-    serverDependenciesToBundle: ['normalize-url'],
-    tailwind: true,
-};
